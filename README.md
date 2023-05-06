d90a4fb2c5f601242a89e9c87b255e2ac31c1db1
Tuan Nguyen <tuanbmt123123@gmail.com>
2023-05-06 15:22:25 +0700

    Deploy

---
+++ README.md
@@ -0,0 +1,8 @@
+# HackYourPortfolio
+
+_Please note: All work on this Repository was built during the [Hack Your Portfolio](https://organize.mlh.io/participants/events/6291-hack-your-portfolio) Challenge hosted by [MLH](https://mlh.io/)_
+
+
+### References
+1. [This](https://www.pyimagesearch.com/2018/06/11/how-to-build-a-custom-face-recognition-dataset/) blog from Pyimagesearch
+2. [This](https://github.com/abhiwalia15/Face-Classification-into-Blood-No-Blood/blob/master/app.py) code file
---
+++ face_detection_streamlit.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import cv2
+
+st.title("Face Detection")
+
+# load OpenCV's Haar cascade for face detection from disk
+detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+# initialize the video stream
+"[INFO] starting video stream..."
+
+
+@st.cache(allow_output_mutation=True)
+def get_cap():
+    return cv2.VideoCapture(0)
+
+cap = get_cap()
+
+frameST = st.empty()
+
+# loop over the frames from the video stream
+while True:
+    ret, frame = cap.read()
+    orig = frame.copy()
+
+    # detect faces in the grayscale frame
+    rects = detector.detectMultiScale(
+        cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), scaleFactor=1.1, 
+        minNeighbors=5, minSize=(30, 30))
+
+    # loop over the face detections and draw them on the frame
+    for (x, y, w, h) in rects:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    # show the output frame
+    frameST.image(frame, channels="BGR")
+    
+        
+    
+        
